054646a336a1384463363be0fc863ef3520fa05a
Daniel Pereyra <danielper.dev@gmail.com>
2023-07-11 18:39:32 -0300

    Estilado de tarjetas, fondo, botones y vista de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Card from './components/Card'
+import { useState } from 'react';
+import './App.css';
+import Card from './components/Card';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     
-    <div className="container">
+    <div className="d-flex flex-wrap container">
       
       <Card/>
-      
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+
+
     </div>
       
   )  
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,19 @@ import "./Card.css";
 
 function Card() {
   return (
-    <div className='card' style={{ width: '18rem' }}>
-    <img src="..." class="card-img-top" alt="..."></img>
-    <div className='card-body'>
-      <h5 className='card-title'>Card title</h5>
-      <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <div className='d-flex justify-content-center'>
-        <button href="#" className='btn btn-primary btn-sm'>Delete</button>
-        <button href="#" className='btn btn-primary btn-sm'>Editar</button>
-        <button href="#" className='btn btn-primary btn-sm'>Delete</button>
-      </div>
-    </div>
+    <div className='card' style={{ width: '15rem' }}>
+        <img src="https://img.ecartelera.com/sagas/000/9.jpg"  className='card-img-top' alt="..."></img>
+            <div className='card-body'>
+                <h4 className='card-title'>Card title</h4>
+                <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                <div className='d-flex justify-content-center'>
+
+                    <button href="#" className='btn btn-primary btn-sm'>Delete</button>
+                    <button href="#" className='btn btn-primary btn-sm'>Editar</button>
+
+                </div>
+            </div>
   </div>
 )
 }
